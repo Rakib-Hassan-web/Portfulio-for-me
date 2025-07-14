688b0b50d0ad0fa5d31dfb808889de3fca67a52c
Rakib-Hassan-web <rakibhassan.web@gmail.com>
2025-07-15 01:05:58 +0600

    banner 2nd text add

--- src/assets/Pages/Banner.jsx
+++ src/assets/Pages/Banner.jsx
@@ -11,9 +11,9 @@ return (
 
         <div className="container">
 
-            <div className="banner_main flex items-center justify-between">
+            <div className="banner_main flex items-center gap-[600px]">
 
-                <div className="bannertexxt">
+                <div className="bannertexxt pl-[50px]">
                     <h2 className=' font-prymary font-bold text-[48px] text-white '>Hi, I am
                     </h2>
                     <span className=' font-prymary font-bold text-[48px] text-white '>Md.Rakib Hasan</span>
@@ -29,8 +29,9 @@ return (
 
 
 
-                <div className="banner_img w-[650px] h-[650px] rounded-full ">
-                    <img className=' overflow-hidden bg-cover  rounded-full bg-center  w-[650px] h-[750px] ' src={bnrimg} alt="" /></div>
+                <div className="banner_img ">
+                    <img className='bg-cover w-[450px] h-[600px] rounded-full' src={bnrimg} alt="" />
+                    </div>
 
 
             </div>
