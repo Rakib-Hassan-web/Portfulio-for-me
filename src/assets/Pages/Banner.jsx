39483d7319e6e366bb53fde17550113c96cc00de
Rakib-Hassan-web <rakibhassan.web@gmail.com>
2025-07-15 00:08:56 +0600

    banner 1st text add

--- src/assets/Pages/Banner.jsx
+++ src/assets/Pages/Banner.jsx
@@ -5,9 +5,32 @@ const Banner = () => {
     <>
     
     
-    <section id='banner'>
+    <section id='banner' className='py-[100px] bg-prymary'>
 
- <div className="container">hi</div>
+ <div className="container">
+
+<div className="banner_main">
+
+<div className="bannertexxt">
+    <h2 className=' font-prymary font-bold text-[48px] text-white '>Hi, I am
+    </h2>
+    <span className=' font-prymary font-bold text-[48px] text-white '>Md.Rakib Hasan</span>
+</div>
+
+
+
+<div className="banner_img"></div>
+
+
+</div>
+
+
+
+
+
+
+
+ </div>
 
 
     </section>
