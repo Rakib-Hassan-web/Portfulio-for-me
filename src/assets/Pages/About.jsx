4d88cf4c938dd9d3dcc7d2b424d3cf7c382e686e
Rakib-Hassan-web <rakibhassan.web@gmail.com>
2025-07-15 01:25:30 +0600

    about part react icon add

--- src/assets/Pages/About.jsx
+++ src/assets/Pages/About.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { SiNamecheap } from 'react-icons/si'
 
 const About = () => {
   return (
@@ -12,7 +13,16 @@ const About = () => {
 
 <h2 id='about_title ' className='text-center text-[48px] font-bold font-prymary text-white mb-[48px]'>About</h2>
 
-<p className='w-[745px]  text-[18px] font-normal font-prymary text-white text-center m-auto'>I am Rakib, a passionate Full-Stake Developer to building dynamic and responsive web applications. Skilled in HTML, HTML5, CSS, CSS3, javascript, React and Tailwind CSS. I help you create websites for your business at affordable cost.  My objective is to deliver work within time and budget that meets the client's requirements.</p>
+<p className='w-[745px]  text-[18px] font-normal font-prymary text-white text-center m-auto pb-10'>I am Rakib, a passionate Full-Stake Developer to building dynamic and responsive web applications. Skilled in HTML, HTML5, CSS, CSS3, javascript, React and Tailwind CSS. I help you create websites for your business at affordable cost.  My objective is to deliver work within time and budget that meets the client's requirements.</p>
+
+<div className="about_Info">
+
+<div className="single_About">
+    <SiNamecheap />
+</div>
+
+
+</div>
 
 </div>
 
