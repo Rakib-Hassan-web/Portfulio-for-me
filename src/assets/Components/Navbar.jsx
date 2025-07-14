67c6a5f011b1a01b857bf9e45dd067bef70861af
Rakib-Hassan-web <rakibhassan.web@gmail.com>
2025-07-14 23:25:12 +0600

    Navbar complete

--- src/assets/Components/Navbar.jsx
+++ src/assets/Components/Navbar.jsx
@@ -8,15 +8,22 @@ const Navbar = () => {
     <section id='navbar' className='py-[40px]  bg-prymary'>
 
 
- <div className="container">
+ <div className="container flex justify-between items-center ">
 
-<h1 className=''>RHR</h1>
-<div id="Navitems">
-<ul><li><Link></Link></li></ul>
-<ul><li><Link></Link></li></ul>
-<ul><li><Link></Link></li></ul>
-<ul><li><Link></Link></li></ul>
+<Link className='text-white  font-bold text-[36px] font-prymary'>RHR</Link>
+<div id="Navitems " >
+<ul className='flex items-center gap-10 text-white text-18px] font-normal font-prymary' >
+    <li className='  p-[10px]  hover:scale-[1.1] hover:text-black hover:rounded-[15px] hover:bg-[#85f994c5] duration-[.4s] '><Link>About me</Link></li>
 
+<li className='  p-[10px]  hover:scale-[1.1] hover:text-black hover:rounded-[15px] hover:bg-[#85f994c5] duration-[.4s] '><Link>Services</Link></li>
+
+
+
+<li className='  p-[10px]  hover:scale-[1.1] hover:text-black hover:rounded-[15px] hover:bg-[#85f994c5] duration-[.4s] '><Link>Skills</Link></li>
+
+<li className='  p-[10px]  hover:scale-[1.1] hover:text-black hover:rounded-[15px] hover:bg-[#85f994c5] duration-[.4s] '><Link>Contact me</Link></li>
+
+</ul>
 </div>
  </div>
 
