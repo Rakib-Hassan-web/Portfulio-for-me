b879929e1e52e7679ed48a7ba7efe8f49b15ecef
Rakib-Hassan-web <rakibhassan.web@gmail.com>
2025-07-15 03:37:28 +0600

    contact 1st input

--- src/assets/Pages/Contract.jsx
+++ src/assets/Pages/Contract.jsx
@@ -22,7 +22,13 @@ return (
             <div className="Contactinp pb-[200px]">
 
                 <div className="firstinpRow flex-row gap-10">
-                  
+                    <input type="text"
+                        className='ml-60 mr-30 py-[18px] pr-[200px]  border-[#4F4F4F]  text-[20px] text-[#d5d5d5] font-prymary font-medium border-b-2 outline-none '
+                        placeholder='First Name' />
+
+
+
+                      
                 </div>
 
 
