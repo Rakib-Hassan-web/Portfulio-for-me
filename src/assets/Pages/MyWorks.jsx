d9083e91d733544d3dfcbaada5d2677708cf1b3f
Rakib-Hassan-web <rakibhassan.web@gmail.com>
2025-10-18 21:27:53 +0600

    my works add

---
+++ src/assets/Pages/MyWorks.jsx
@@ -0,0 +1,45 @@
+import React from 'react'
+
+import { FaCode, FaLaptopCode, FaPaintBrush, FaMobileAlt, FaUsers, FaLightbulb } from "react-icons/fa";
+
+const MyWorks = () => {
+
+
+    
+  return (
+    <>
+    
+    
+
+  <section className="bg-prymary  text-white py-26 mb-0 px-6">
+      <h2 className="text-4xl font-bold text-center mb-12">What I Do</h2>
+
+      <div className="grid md:grid-cols-3 sm:grid-cols-2 gap-8 max-w-6xl mx-auto">
+        {services.map((service, index) => (
+          <div
+            key={index}
+            className="bg-gray-900 border border-green-700 rounded-2xl p-6 text-center 
+                       shadow-md hover:shadow-green-600 hover:-translate-y-2 
+                       transition duration-300 ease-in-out"
+          >
+            <div className="flex justify-center mb-4">{service.icon}</div>
+            <h3 className="text-xl font-semibold mb-2">{service.title}</h3>
+            <p className="text-gray-400 text-sm">{service.desc}</p>
+          </div>
+        ))}
+      </div>
+    </section>
+ 
+    
+    
+    
+    
+    
+    
+    
+    
+    </>
+  )
+}
+
+export default MyWorks
