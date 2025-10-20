ed6a1e00a0fc396adaf819efc75b79551b979d96
Rakib-Hassan-web <rakibhassan.web@gmail.com>
2025-10-20 23:33:54 +0600

    designing my works section 1st cart complete

--- src/assets/Pages/MyWorks.jsx
+++ src/assets/Pages/MyWorks.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 
 import { FaCode, FaLaptopCode, FaPaintBrush, FaMobileAlt, FaUsers, FaLightbulb } from "react-icons/fa";
+import { Link } from 'react-router';
 
 const MyWorks = () => {
 
@@ -19,15 +20,16 @@ const MyWorks = () => {
       <div>
 
 
-
+{/* --------------1st cart------- */}
 
         <div  className=' w-70 h-100 bg-blue-300 rounded-2xl'>
+          <Link>
           <img src="#" alt="image" className='bg-gray-600 w-full h-70 rounded-2xl ' />
           <div>
             <h2>project name</h2>
             <h2>whic elements ar use in this project ( like : html ,css ,react ,js)</h2>
           </div>
-               
+               </Link>
         </div>
 
 
